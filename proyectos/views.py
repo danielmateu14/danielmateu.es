45f64fce5f3da5ab26dafec49035fab8ec5afc13
Daniel Mateu <mateudani97@gmail.com>
2025-07-11 13:02:44 +0200

    Prueba2 fallo 500 render

--- proyectos/views.py
+++ proyectos/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import ListView, DetailView
 from .models import Proyecto
 
+from django.views.generic import ListView, DetailView
+from .models import Proyecto
+
 class ProyectoListView(ListView):
     model = Proyecto
     template_name = 'proyecto_list.html'
@@ -10,9 +13,18 @@ class ProyectoListView(ListView):
     def get_queryset(self):
         try:
             return Proyecto.objects.all().order_by('-fecha_creacion')
-        except Exception as e:
-            # Puedes registrar el error aquí si usas logging
+        except Exception:
+            # Si hay algún fallo al consultar (por ejemplo, tabla vacía o fallo de relaciones), evita el error 500
             return Proyecto.objects.none()
+
+
+class ProyectoDetalleView(DetailView):
+    model = Proyecto
+    template_name = 'proyecto_detalle.html'
+    context_object_name = 'proyecto'
+    slug_field = 'slug'
+    slug_url_kwarg = 'pk'
+
 
 
 class ProyectoDetalleView(DetailView):
