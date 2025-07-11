0d9856e47581dc8603c3a54d82a3b9a3cfa1c59f
Daniel Mateu <mateudani97@gmail.com>
2025-07-11 13:17:23 +0200

    Prueba 4. Creo proyectos

--- proyectos/views.py
+++ proyectos/views.py
@@ -11,12 +11,7 @@ class ProyectoListView(ListView):
     paginate_by = 6
 
     def get_queryset(self):
-        try:
-            return Proyecto.objects.all().order_by('-fecha_creacion')
-        except Exception:
-            # Si hay algún fallo al consultar (por ejemplo, tabla vacía o fallo de relaciones), evita el error 500
-            return Proyecto.objects.none()
-
+        return Proyecto.objects.all().order_by('-fecha_creacion')
 
 class ProyectoDetalleView(DetailView):
     model = Proyecto
@@ -25,11 +20,3 @@ class ProyectoDetalleView(DetailView):
     slug_field = 'slug'
     slug_url_kwarg = 'pk'
 
-
-
-class ProyectoDetalleView(DetailView):
-    model = Proyecto
-    template_name = 'proyecto_detalle.html'
-    context_object_name = 'proyecto'
-    slug_field = 'slug'
-    slug_url_kwarg = 'pk'
