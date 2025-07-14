cdfd226ed7a8205f44221a35f2d18844f1621bf0
Daniel Mateu <mateudani97@gmail.com>
2025-07-14 15:37:38 +0200

    prueba produccion

--- home/views.py
+++ home/views.py
@@ -5,3 +5,5 @@ def inicio(request):
     proyectos_destacados = Proyecto.objects.order_by('-fecha_creacion')[:3]
     return render(request, 'home/inicio.html', {'proyectos_destacados': proyectos_destacados})
 
+def inic():
+    return
