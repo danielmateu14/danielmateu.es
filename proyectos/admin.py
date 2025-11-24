945b295bca67bb664c8a6d068405d15d4f3dc558
Daniel Mateu <mateudani97@gmail.com>
2025-11-24 21:54:33 +0100

    Registrar modelos Proyecto y Tecnologia en admin

--- proyectos/admin.py
+++ proyectos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Proyecto, Tecnologia
 
-# Register your models here.
+@admin.register(Tecnologia)
+class TecnologiaAdmin(admin.ModelAdmin):
+    list_display = ['nombre']
+    search_fields = ['nombre']
+
+@admin.register(Proyecto)
+class ProyectoAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'fecha_creacion']
+    list_filter = ['tecnologias', 'fecha_creacion']
+    search_fields = ['titulo', 'descripcion']
+    prepopulated_fields = {'slug': ('titulo',)}
+    filter_horizontal = ['tecnologias']
